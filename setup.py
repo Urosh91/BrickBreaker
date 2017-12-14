f90c0bd2d9c5c3834dcccde1dbec443f9cfa9473
Uroš Jevremović <jevremovic.uros91@gmail.com>
2017-12-14 22:01:54 +0100

    Fixed bug with path leading to data

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 
 setup(name="BrickBreakerGame",
-      version="0.1.3",
+      version="0.1.4",
       description="Brick Breaker game",
       long_description=__doc__,
       url="https://github.com/Urosh91/BrickBreaker",
