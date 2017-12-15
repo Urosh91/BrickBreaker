9937ede8fb39f78da7a0fb9c1b762b6069d123e5
Uroš Jevremović <jevremovic.uros91@gmail.com>
2017-12-16 00:06:21 +0100

    fixed path to highscore file based on OS

--- BrickBreaker/highscore.py
+++ BrickBreaker/highscore.py
@@ -1,9 +1,9 @@
 import fileinput
 import hashlib
 import operator
-from pkg_resources import resource_filename
 import getpass
 import os
+import platform
 
 
 class Highscore:
@@ -18,20 +18,56 @@ class Highscore:
     def load():
         user = getpass.getuser()
         highscore = []
-        try:
-            f = open(f"/home/{user}/highscore.dat", "r")
-        except IOError:
-            f = open(f"/home/{user}/highscore.dat", "w")
-        for line in fileinput.input(f"/home/{user}/highscore.dat"):
-            name, score, md5 = line.split('[::]')
-            md5 = md5.replace('\n', '')
+        if platform.system() == "Linux":
+            try:
+                f = open(f"/home/{user}/highscore.dat", "r")
+            except IOError:
+                f = open(f"/home/{user}/highscore.dat", "w")
+            for line in fileinput.input(f"/home/{user}/highscore.dat"):
+                name, score, md5 = line.split('[::]')
+                md5 = md5.replace('\n', '')
 
-            if str(hashlib.md5(str.encode(str(name+score+"pygame"))).hexdigest()) == str(md5):
-                highscore.append([str(name), int(score), str(md5)])
+                if str(hashlib.md5(str.encode(str(name+score+"pygame"))).hexdigest()) == str(md5):
+                    highscore.append([str(name), int(score), str(md5)])
 
-        highscore.sort(key=operator.itemgetter(1), reverse=True)
-        highscore = highscore[0:10]
-        f.close()
+            highscore.sort(key=operator.itemgetter(1), reverse=True)
+            highscore = highscore[0:10]
+
+            f.close()
+
+        elif platform.system() == "Windows":
+            try:
+                f = open(f"{os.path.abspath(os.sep)[:-1]}highscore.dat", "r")
+            except IOError:
+                f = open(f"{os.path.abspath(os.sep)[:-1]}highscore.dat", "w")
+            for line in fileinput.input(f"{os.path.abspath(os.sep)[:-1]}highscore.dat"):
+                name, score, md5 = line.split('[::]')
+                md5 = md5.replace('\n', '')
+
+                if str(hashlib.md5(str.encode(str(name + score + "pygame"))).hexdigest()) == str(md5):
+                    highscore.append([str(name), int(score), str(md5)])
+
+            highscore.sort(key=operator.itemgetter(1), reverse=True)
+            highscore = highscore[0:10]
+
+            f.close()
+
+        elif platform.system() == "Darwin":
+            try:
+                f = open(f"/Users/{user}/highscore.dat", "r")
+            except IOError:
+                f = open(f"/Users/{user}/highscore.dat", "w")
+            for line in fileinput.input(f"/Users/{user}/highscore.dat"):
+                name, score, md5 = line.split('[::]')
+                md5 = md5.replace('\n', '')
+
+                if str(hashlib.md5(str.encode(str(name + score + "pygame"))).hexdigest()) == str(md5):
+                    highscore.append([str(name), int(score), str(md5)])
+
+            highscore.sort(key=operator.itemgetter(1), reverse=True)
+            highscore = highscore[0:10]
+
+            f.close()
 
         return highscore
 
@@ -40,7 +76,18 @@ class Highscore:
         hash = hashlib.md5((str(name+str(score)+"pygame")).encode("utf-8"))
         self._highscore.append([name, str(score), hash.hexdigest()])
 
-        with open(f"/home/{user}/highscore.dat", "w") as f:
-            for name, score, md5 in self._highscore:
-                f.write(str(name)+"[::]"+str(score)+"[::]"+str(md5)+"\n")
+        if platform.system() == "Linux":
+            with open(f"/home/{user}/highscore.dat", "w") as f:
+                for name, score, md5 in self._highscore:
+                    f.write(str(name)+"[::]"+str(score)+"[::]"+str(md5)+"\n")
+
+        elif platform.system() == "Windows":
+            with open(f"{os.path.abspath(os.sep)[:-1]}highscore.dat", "w") as f:
+                for name, score, md5 in self._highscore:
+                    f.write(str(name) + "[::]" + str(score) + "[::]" + str(md5) + "\n")
+
+        elif platform.system() == "Darwin":
+            with open(f"/Users/{user}/highscore.dat", "w") as f:
+                for name, score, md5 in self._highscore:
+                    f.write(str(name) + "[::]" + str(score) + "[::]" + str(md5) + "\n")
 
